987b302d532a79b8b74194e15b9a4cd4942ca82c
IronicBadger <alexktz@gmail.com>
2018-07-17 18:42:57 +0100

    configured keypair and SGs

--- rhel7/rhel7.tf
+++ rhel7/rhel7.tf
@@ -19,11 +19,32 @@ data "aws_ami" "rhel7_ami" {
     owners = ["309956199498"] # red hat
 }
 
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }    
+}
+
 resource "aws_instance" "rhel7" {
     count = 3
 
     ami = "${data.aws_ami.rhel7_ami.id}"
     instance_type = "t2.micro"
+    key_name = "neo"
+    vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
 
     tags {
         Name = "TEST-${count.index + 1}"
